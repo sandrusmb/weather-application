8ce98762827d3167806a89863a051e8bc968a70f
sandrusmb <sandrusmb@gmail.com>
2020-02-12 01:21:46 +0100

    Add docs folder again

---
+++ docs/precache-manifest.2680e32a830a4100f7a77301524ee7d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7de5e6e09a70cef5d6bb7b216b00ba8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9394a904060a42cf258",
+    "url": "./static/css/main.b03d47dd.chunk.css"
+  },
+  {
+    "revision": "c1e72ee03b9aa7804663",
+    "url": "./static/js/2.37395498.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.37395498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9394a904060a42cf258",
+    "url": "./static/js/main.945a7824.chunk.js"
+  },
+  {
+    "revision": "873befb6a8bd974a5d71",
+    "url": "./static/js/runtime-main.953bed08.js"
+  }
+]);
